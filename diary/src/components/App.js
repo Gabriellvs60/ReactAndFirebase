d27cecc1e3787c0830e1b9c1f3b07295d4ed72f3
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-30 20:06:24 -0300

    Configurando Redux e Fazendo Get no Firebase
    Configurando Redux e Fazendo Get no Firebase

--- diary/src/components/App.js
+++ diary/src/components/App.js
@@ -1,58 +1,109 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { database } from '../firebase';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { getNotes, saveNote } from '../actions/notesAction';
+
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    // state
     this.state = {
-        title: '',
-        body: ''
+      title: '',
+      body: '',
+      notes: {}
     };
-    //bind
-    this.handleChange = this.handleChange.bind(this)
+
+    // bind
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderNotes = this.renderNotes.bind(this);
   }
 
-  handleChange(e){
+  // lifecycle
+  componentDidMount() {
+    this.props.getNotes();
+  }
+
+  // handle change
+  handleChange(e) {
     this.setState({
-      [e.target.name]:e.target.value
+      [e.target.name]: e.target.value
     });
-}
+  }
 
-render(){
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-sm-6 col-sm-offset-3">
-          <form>
-            <div className="form-group">
-              <input
-              onChange={this.handleChange}
-              type="text"
-              name="title"
-              className="form-control no-border"
-              placeholder="Title..."
-              required/>
-            </div>
+  // handle submit
+  handleSubmit(e) {
+    e.preventDefault();
+    const note = {
+      title: this.state.title,
+      body: this.state.body
+    };
 
-            <div className="form-group">
-              <input
-              onChange={this.handleChange}
-              type="text"
-              name="body"
-              className="form-control no-border"
-              placeholder="Body..."
-              required/>
-            </div>
+    this.props.saveNote();
+    this.setState({
+      title: '',
+      body: ''
+    });
+  }
 
-            <div className="form-group">
-              <button className="btn btn-primary col-sm-12">Save</button>
-            </div>
+  // render notes
+  renderNotes() {
+    return _.map(this.state.notes, (note, key) => {
+      return (
+        <div key="key">
+          <h2>{note.title}</h2>
+          <p>{note.body}</p>
+        </div>
+      );
+    });
+  }
 
-          </form>
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <input
+                  onChange={this.handleChange}
+                  value={this.state.title}
+                  type="text"
+                  name="title"
+                  className="form-control no-border"
+                  placeholder="Title..."
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <textarea
+                  onChange={this.handleChange}
+                  value={this.state.body}
+                  type="text"
+                  name="body"
+                  className="form-control no-border"
+                  placeholder="Body..."
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <button className="btn btn-primary col-sm-12">Save</button>
+              </div>
+            </form>
+            {this.renderNotes()}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state, ownProps) {
+  return {
+    notes: state.notes
+  };
+}
+
+export default connect(mapStateToProps, { getNotes, saveNote })(App);
