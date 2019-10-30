56897e71e90740023092605f55ef6357e7c44886
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-30 15:33:31 -0300

    Initial commit

---
+++ diary/src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase'
+  var firebaseConfig = {
+    apiKey: "AIzaSyAMFS81_nl6oQBJgS198ejug6ScKUBkQ1E",
+    authDomain: "diary-d8346.firebaseapp.com",
+    databaseURL: "https://diary-d8346.firebaseio.com",
+    projectId: "diary-d8346",
+    storageBucket: "diary-d8346.appspot.com",
+    messagingSenderId: "565140012316",
+    appId: "1:565140012316:web:6a6e7c2195014d8b3f0ed9",
+    measurementId: "G-GT6J0XM78L"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export const database = firebase.database().ref('/notes');
