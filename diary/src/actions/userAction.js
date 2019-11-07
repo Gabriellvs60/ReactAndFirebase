6a4eb4700bea8808b3972e6c1ab1e731c1ddc631
Gabriel Coelho <gabriellvs60@gmail.com>
2019-11-07 16:40:47 -0300

    Managing Notes and user status
    Managing Notes and user status
    
    Redux -> Actions

--- diary/src/actions/userAction.js
+++ diary/src/actions/userAction.js
@@ -1,12 +1,22 @@
 import {auth, googleProvider, twitterProvider} from '../firebase';
-import {GET_USER} from '../actionTypes'
+import {GET_USER, USER_STATUS} from '../actionTypes'
 
 export function getUser(){
     return dispatch => {
+        //SHOW LOADING STATUS BEFORE GETTING USER TO TRUE
+        dispatch({
+            type: USER_STATUS,
+            payload: true
+        });
         auth.onAuthStateChanged(user => {
             dispatch({
                 type: GET_USER,
                 payload : user
+            });
+            //show loading status to false
+            dispatch({
+                type: USER_STATUS,
+                payload: false
             })
         });
     }
